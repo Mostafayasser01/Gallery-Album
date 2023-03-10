39527e109901ad4a957c5acf2ef5d7ff971d71b0
Mostafayasser01 <86493096+Mostafayasser01@users.noreply.github.com>
2023-03-10 02:30:15 +0200

    Add files via upload

---
+++ Gallery Album/JS/test.js
@@ -0,0 +1,5 @@
+function changecolor(){
+    var arrcolor=new Array("#ff5733", "#DAF7A6" ,"#FFC300" ,"#FF5733" ,"#C70039" ,"#900C3F" ,"#581845" );
+    document.body.style.background=arrcolor[Math.floor(Math.random()*arrcolor.length)]    
+}
+
